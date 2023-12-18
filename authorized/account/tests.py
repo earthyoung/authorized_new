2cde8a683bf5164e36d8d8e834a9ab0c7ee1b9d1
earthyoung <studydatawithme@gmail.com>
2023-12-18 12:38:28 +0900

    feat: account test PermissionTest 추가

--- authorized/account/tests.py
+++ authorized/account/tests.py
@@ -1,12 +1,13 @@
 from django.test import TestCase
 from account.models import *
+from account.exception import *
 
 
 # Create your tests here.
 class UserTest(TestCase):
     def test_user_provider_is_in_range(self):
         user = User.objects.create(email="xnkjxnfkdsnf@gmail.com")
-        self.assertIn(user.provider, User.Provider.values)
+        self.assertEqual(user.provider, User.Provider.COMMON.value)
 
     def test_user_email_google(self):
         user = User.signup_manager.create(email="dshfksafj@gmail.com", username="111")
@@ -15,3 +16,21 @@ class UserTest(TestCase):
     def test_user_email_others(self):
         user = User.signup_manager.create(email="sjkafnkjsf@naver.com", username="222")
         self.assertEqual(user.provider, User.Provider.KAKAO.value)
+
+
+class PermissionTest(TestCase):
+    def test_health_api(self):
+        response = self.client.get("/account/health/", follow=True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_user_api_without_jwt(self):
+        def call_user():
+            self.client.get("/account/user/")
+
+        self.assertRaises(JwtNotExistException, call_user)
+
+    def test_logout_api_without_jwt(self):
+        def call_api():
+            self.client.post("/account/logout/")
+
+        self.assertRaises(JwtNotExistException, call_api)
