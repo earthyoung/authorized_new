fe883f4ffb105d5d18aed37812f0cf8691b4d5d2
earthyoung <studydatawithme@gmail.com>
2024-01-03 23:33:29 +0900

    feat: user group list + FE 연동

--- authorized/account/urls.py
+++ authorized/account/urls.py
@@ -7,6 +7,7 @@ from django.urls import path
 urlpatterns = [
     path("health/", HealthView.as_view()),
     path("user/", UserViewSet.as_view({"get": "retrieve", "post": "destroy"})),
+    path("group/", GroupViewSet.as_view({"get": "list"})),
     path("google/login/", GoogleLoginView.as_view()),
     path("kakao/login/", KakaoLoginView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
