d514853a1f7707d809d435771e948ad1576a728c
earthyoung <studydatawithme@gmail.com>
2024-01-08 17:00:41 +0900

    feat: log remove model Chat, Channel

--- authorized/log/models.py
+++ authorized/log/models.py
@@ -3,24 +3,24 @@ from account.models import User, Group
 
 
 # Create your models here.
-class TimeStamp(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+# class TimeStamp(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
-    class Meta:
-        abstract = True
-
-
-class Channel(models.Model):
-    members = models.ManyToManyField(User)
-    created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
-    updated_at = models.DateTimeField(null=True, blank=True, auto_now=True)
+#     class Meta:
+#         abstract = True
 
 
-class Chat(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
-    content = models.TextField(null=True, blank=True)
+# class Channel(models.Model):
+#     members = models.ManyToManyField(User)
+#     created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+#     updated_at = models.DateTimeField(null=True, blank=True, auto_now=True)
+
+
+# class Chat(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+#     channel = models.ForeignKey(Channel, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(null=True, blank=True, auto_now_add=True)
+#     content = models.TextField(null=True, blank=True)
 
 
 class EmailLog(models.Model):
